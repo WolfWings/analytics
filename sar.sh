e74a5aa8eae98a9e7bd672b4e32d0d25453cf4cb
Wolf Armstrong <wolf.armstrong@rackspace.com>
2013-04-18 15:31:53 -0500

    Rebuilding w/ better function naming and structure, as well as initial commentary for some internal functions.

--- sar.sh
+++ sar.sh
@@ -19,16 +19,77 @@ esac; done
 #   periods so they don't pollute the statistics.
 
 LC_ALL="POSIX" sar -r -n DEV $SARFILE | egrep "^[A0-9][v0-9][e:][r0-9][a0-9][g:][e0-9][0-9:]" | grep -v "RESTART" | awk '
+function is_tier_item(item) {
+}
+
+function values(item, value) {
+	if (item ~ /^_[lmu]_/) {
+		return;
+	}
+	if (value != "") {
+		if (item in values_storage) {
+			values_storage[item] = (values_storage[item] SUBSEP value);
+		} else {
+			values_storage[item] = value;
+		}
+	} else if (item != "") {
+		return values_storage[item];
+	} else {
+		return;
+	}
+}
+
+# Calculates the standard-deviation values.
 function stddev(item, value) {
 	if (value != "") {
 		stddev_base[item] += value;
 		stddev_sqrt[item] += (value ** 2);
 		stddev_size[item] += 1;
+		values(item, value);
 	} else {
 		if (stddev_size[item] > 0) {
 			return sqrt(stddev_sqrt[item]/stddev_size[item] - (mean(item) ** 2));
 		} else {
 			return log(0);
+		}
+	}
+}
+
+# This divides an item in values_storage into thirds,
+# recalculating stddev for the upper, middle, and the
+# lower third tiers using prefixes of _u_, _m_, & _l_
+# which are also checked to avoid runaway memory use.
+#
+# The algo. is trivial and brute-force:
+#   1) Scan all values once to find min/max.
+#   2) Calculate breakpoints at the thirds.
+#   3) Scan again to bin values appropriately.
+function values_tiers_calculate(item) {
+	if (item ~ /^_[lmu]_/) {
+		return;
+	}
+	split(values(item), v, SUBSEP);
+	min[item] = -log(0);
+	max[item] = 0;
+	for (j in v) {
+		k = v[j];
+		if (k < min[item]) {
+			min[item] = k;
+		}
+		if (k > max[item]) {
+			max[item] = k;
+		}
+	}
+	upper = (max[item] + max[item] + min[item]) / 3;
+	lower = (max[item] + min[item] + min[item]) / 3;
+	for (j in v) {
+		k = v[j];
+		if (k < lower) {
+			stddev(("_l_" item), k);
+		} else if (k <=upper) {
+			stddev(("_m_" item), k);
+		} else {
+			stddev(("_u_" item), k);
 		}
 	}
 }
@@ -41,15 +102,28 @@ function mean(item) {
 	}
 }
 
-function values(item, value) {
-	if (value != "") {
-		if (item in values_storage) {
-			values_storage[item] = (values_storage[item] SUBSEP value);
-		} else {
-			values_storage[item] = value;
-		}
+function output_network_range(item,   mean, range) {
+	mean = mean(item);
+	range = stddev(item);
+	if ((mean > log(0)) && (mean < -log(0))) {
+		printf "%8.2f      ", mean;
 	} else {
-		return values_storage[item];
+		printf "        .       ";
+	}
+	if ((range > log(0)) && (range < -log(0))) {
+		printf "%8.2f   ", range;
+	} else {
+		printf "        .     ";
+	}
+}
+
+function output_network_tier(class, title, device) {
+	if ((stddev_size[(class "rx_" device)] > 0) ||
+	    (stddev_size[(class "tx_" device)] > 0)) {
+		printf "%6s %5s   ", i, title;
+		output_network_range((class "rx_" device));
+		output_network_range((class "tx_" device));
+		printf "\n";
 	}
 }
 
@@ -73,8 +147,6 @@ BEGIN {
 	} else {
 		stddev(("rx_" $2), $5);
 		stddev(("tx_" $2), $6);
-		values(("rx_" $2), $5);
-		values(("tx_" $2), $6);
 		ifaces[$2] = 1;
 	}
 }
@@ -89,56 +161,29 @@ BEGIN {
 
 END {
 	printf "Memory Stats================================================================\n";
-	printf "             Average       Std.Dev.\n";
+	printf "             Average    Plus/Minus\n";
 	printf "Used     %9.2fGB   %9.2fGB\n", mean("mem_u"), stddev("mem_u");
 	printf "Buffer   %9.2fGB   %9.2fGB\n", mean("mem_b"), stddev("mem_b");
 	printf "Cache    %9.2fGB   %9.2fGB\n", mean("mem_c"), stddev("mem_c");
 
 	for (i in values_storage) {
-		split(values(i), v, SUBSEP);
-		min[i] = -log(0);
-		max[i] = 0;
-		for (j in v) {
-			k = v[j];
-			if (k < min[i]) {
-				min[i] = k;
-			}
-			if (k > max[i]) {
-				max[i] = k;
-			}
-		}
-		upper = (max[i] + max[i] + min[i]) / 3;
-		lower = (max[i] + min[i] + min[i]) / 3;
-		for (j in v) {
-			k = v[j];
-			if (k < lower) {
-				stddev(("_l_" i), k);
-			} else if (k <=upper) {
-				stddev(("_m_" i), k);
-			} else {
-				stddev(("_u_" i), k);
-			}
-		}
+		values_tiers_calculate(i);
 	}
+
 	printf "Network Stats===============================================================\n";
-	printf "                          R                        T\n";
-	printf "    Device       Average     Std.Dev.     Average     Std.Dev    Summed Peak\n";
+	printf "                        Rx                       Tx\n";
+	printf "    Device      Average    Plus/Minus    Average    Plus/Minus       Peak Tx\n";
 	for (i in ifaces) {
 		if ((stddev_base[("rx_" i)] >= (NR / 100)) ||
 		    (stddev_base[("tx_" i)] >= (NR / 100))) {
-			printf "%6s Total   %8.2f    %8.2f     %8.2f    %8.2f   %5.2fMBit/sec\n", i, mean(("rx_" i)), stddev(("rx_" i)), mean(("tx_" i)), stddev(("tx_" i)), (max[("tx_" i)] + max[("rx_" i)]) / 128;
-			if ((stddev_size[("_l_rx_" i)] > 0) ||
-			    (stddev_size[("_l_tx_" i)] > 0)) {
-				printf "%6s Lower   %8.2f    %8.2f     %8.2f    %8.2f\n", i, mean(("_l_rx_" i)), stddev(("_l_rx_" i)), mean(("_l_tx_" i)), stddev(("_l_tx_" i));
-			}
-			if ((stddev_size[("_m_rx_" i)] > 0) ||
-			    (stddev_size[("_m_tx_" i)] > 0)) {
-				printf "%6s Midd.   %8.2f    %8.2f     %8.2f    %8.2f\n", i, mean(("_m_rx_" i)), stddev(("_m_rx_" i)), mean(("_m_tx_" i)), stddev(("_m_tx_" i));
-			}
-			if ((stddev_size[("_u_rx_" i)] > 0) ||
-			    (stddev_size[("_u_tx_" i)] > 0)) {
-				printf "%6s Upper   %8.2f    %8.2f     %8.2f    %8.2f\n", i, mean(("_u_rx_" i)), stddev(("_u_rx_" i)), mean(("_u_tx_" i)), stddev(("_u_tx_" i));
-			}
+			printf "%6s Total   ", i;
+			output_network_range(("rx_" i));
+			output_network_range(("tx_" i));
+			printf "%7.2fMbps\n", max[("tx_" i)] / 1024;
+
+			output_network_tier("_l_", "Lower", i);
+			output_network_tier("_m_", "Midd.", i);
+			output_network_tier("_u_", "Upper", i);
 		}
 	}
 }'
