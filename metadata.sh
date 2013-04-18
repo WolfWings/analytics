7d35135ed91ca58b4a6ee777738084189cdec313
Wolf Armstrong <wolf.armstrong@rackspace.com>
2013-04-18 16:10:29 -0500

    List components in CloudControl order, and migrate 'region' to a menu list instead of a free-text field to avoid errors.

--- metadata.sh
+++ metadata.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
-while /bin/true; do
-	if [ $0 != ${SHELL##*/} ]; then
-		echo "This script must be called via the 'source' command,"
-		echo "to allow it to setup the environmental variables for"
-		echo "the 'nova' client properly."
-		break
-	fi
-	export NOVA_SERVICE_NAME=cloudServersOpenStack
-	export NOVA_URL=https://identity.api.rackspacecloud.com/v2.0/ 
-	export NOVA_VERSION=1.1
-	export NOVA_RAX_AUTH=1
-	dialog \
+export NOVA_SERVICE_NAME=cloudServersOpenStack
+export NOVA_URL=https://identity.api.rackspacecloud.com/v2.0/ 
+export NOVA_VERSION=1.1
+export NOVA_RAX_AUTH=1
+DIALOG=${DIALOG=dialog}
+${DIALOG} \
 	--backtitle "Rackspace Metadata Updater" \
 	--title "API Information" \
 	--no-mouse \
-	--form "\nAccount Details" 0 0 4 \
-	"  Username:" 1 2 "" 1 14 32  0 \
-	" API Token:" 2 2 "" 2 14 32  0 \
-	"Account ID:" 3 2 "" 3 14 32 10 \
-	"    Region:" 4 2 "" 4 14 32  4 \
-	2>/tmp/chri5631.metadata.$$
-	exec 4<>/tmp/chri5631.metadata.$$
-	export NOVA_USERNAME
-	export NOVA_API_KEY
-	export NOVA_PROJECT_ID
-	export NOVA_REGION_NAME
-	read -u 4 NOVA_USERNAME
-	read -u 4 NOVA_API_KEY
-	read -u 4 NOVA_PROJECT_ID
-	read -u 4 NOVA_REGION_NAME
-	break
-done
+	--form "Account Details" 0 0 3 \
+		"Account ID:" 1 2 "" 1 14 32 10 \
+		"  Username:" 2 2 "" 2 14 32  0 \
+		"   API Key:" 3 2 "" 3 14 32  0 \
+	--and-widget \
+	--menu "Region/Datacenter" 0 0 3 \
+		"DFW" "Dallas (US)" \
+		"ORD" "Chicago (US)" \
+		"LON" "London (UK)" \
+	2>>/tmp/chri5631.metadata.$$
+if [ $? -ne 0 ]; then clear; exit; fi
+exec 4<>/tmp/chri5631.metadata.$$
+export NOVA_USERNAME
+export NOVA_API_KEY
+export NOVA_PROJECT_ID
+export NOVA_REGION_NAME
+read -u 4 NOVA_API_KEY
+read -u 4 NOVA_USERNAME
+read -u 4 NOVA_PROJECT_ID
+read -u 4 NOVA_REGION_NAME
+clear
+echo "Finished setting up NOVA."
+echo "Press Ctrl+D or type 'exit' to leave the sub-environment."
+exec ${SHELL}
