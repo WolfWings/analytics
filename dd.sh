5d604e1a4629140a07bc2b2f82e3545ac9e8c409
Wolf Armstrong <wolf.armstrong@rackspace.com>
2013-07-31 14:53:01 -0500

    It would help if the disk-test was ACTUALLY random-seeking instead of linear.

--- dd.sh
+++ dd.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 mkdir /tmp/$$
 (time sh -c "dd if=/dev/zero of=/tmp/$$/source oflag=direct bs=1M count=1000 && sync") 2>&1 | grep "real"
-for SKIP in $(seq 0 1000); do
-	printf "%i.%i%%\r" $[${SKIP}/10] $[${SKIP}%10]
-	(time sh -c "dd if=/tmp/$$/source of=/tmp/$$/target conv=notrunc oflag=direct,append bs=1M count=1 skip=${SKIP}") 2>&1 | grep "real" >> /tmp/$$/log
+for ROUND in $(seq 0 1000); do
+	printf "%i.%i%%\r" $[${ROUND}/10] $[${ROUND}%10]
+	(time sh -c "dd if=/tmp/$$/source of=/tmp/$$/target conv=notrunc oflag=direct,append bs=1M count=1 skip=$[${RANDOM}%1000]") 2>&1 | grep "real" >> /tmp/$$/log
 done
-cat /tmp/$$/log | cut -f 2 | cut -d m -f 2 | cut -d s -f 1 | awk '{sum+=$1} END {print sum, "minutes"}'
+cat /tmp/$$/log | cut -f 2 | cut -d m -f 2 | cut -d s -f 1 | awk '{sum+=$1} END {print sum, "seconds"}'
 rm -rf /tmp/$$
